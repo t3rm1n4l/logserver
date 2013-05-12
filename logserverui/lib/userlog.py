afce29d5ccb6b0893b235a453c70a30763a9ffc8
Sarath Lakshman <sarathlakshman@slynux.com>
2013-05-12 17:28:48 +0530

    Implement view and backend

--- logserverui/lib/userlog.py
+++ logserverui/lib/userlog.py
@@ -1,32 +1,71 @@
 #!/usr/bin/env python
 
 import esclient
+import datetime
+
+def genquery(uid, recent_hrs=0, sort=False, offset=None, size=None):
+        qbody = {
+                "query": {
+                    "filtered" : {
+                        "query": {
+                            "term" : { "uid" : uid }
+                        }
+                    }
+                }
+        }
+
+        if recent_hrs:
+            timestamp = datetime.datetime.now() - datetime.timedelta(hours=recent_hrs)
+            print timestamp.strftime("%Y-%m-%dT%H:%M:%S")
+
+            timefilter = {
+                    "filter" : {
+                        "range" : {
+                            "@timestamp" : {
+                                "from" : timestamp.strftime("%Y-%m-%dT%H:%M:%S")
+                                }
+                        }
+                    }
+            }
+
+            qbody["query"]["filtered"].update(timefilter)
+
+        if offset:
+            qbody["from"] = offset
+
+        if size:
+            qbody["size"] = size
+
+        if sort:
+            qbody["sort"] = [
+                    {"@timestamp" : "desc"},
+            ]
+
+        return qbody
+
 
 class UserLogFetcher:
     def __init__(self, esurl="http://localhost:9200"):
         self._es = esclient.ESClient(esurl)
 
-    def get_userlog(self, uid):
+    def get_userlog(self, uid, hours, offset=None, size=None):
         messages = []
-        qbody = {
-                "query": {
-                    "term" : { "uid" : uid }
-                }
-            }
-    
-        result = self._es.search(query_body=qbody)
+        query = genquery(uid, hours, True, offset, size)
+        result = self._es.search(query_body=query)
+        total = result['hits']['total']
         result = result["hits"]["hits"]
         for log in result:
             data = log['_source']['@fields']
-            messages.append((data['timestamp'], data['data']))
+            messages.append((data['timestamp'][0], data['data'][0]))
 
-        return messages
+        return total, messages
 
 if __name__ == '__main__':
     ulog = UserLogFetcher('http://172.16.21.139:9200')
     for uid in [123, 103]:
-        print "Userlogs for uid", uid
-        for i in ulog.get_userlog(uid):
+        count, msgs = ulog.get_userlog(uid)
+        print "Userlogs for uid %d and count is %d" %(uid, count)
+        for i in msgs:
             print i
 
 
--- logserverui/views.py
+++ logserverui/views.py
@@ -1,6 +1,58 @@
 from django.http import HttpResponse
 from django.shortcuts import render_to_response
+from logserverui.lib.userlog import UserLogFetcher
+
+page_limit = 5
 
 def index(request):
-    return render_to_response('index.html', {})
+    uid = 0
+    page = 0
+    hours = 0
+
+    current_uid = "user id"
+    current_hours = "hours"
+
+    if request.GET.has_key('uid'):
+        uid = int(request.GET['uid'])
+
+    if request.GET.has_key('page'):
+        page = int(request.GET['page'])
+
+    if request.GET.has_key('hours'):
+        hours = int(request.GET['hours'])
+
+    if uid:
+        current_uid = "%d" %uid
+
+    if hours:
+        current_hours = "%d" %hours
+
+    ufetcher = UserLogFetcher('http://172.16.21.139:9200')
+    count, messages = ufetcher.get_userlog(uid, hours, page*page_limit, page_limit)
+
+    maxpages = count/page_limit
+    if count % page_limit:
+        maxpages += 1
+    pages = []
+
+    for i in xrange(maxpages):
+        enable_str = "enabled"
+        if page == i:
+            enable_str = "disabled"
+
+        pages.append((i+1, "?uid=%d&hours=%d&page=%d" %(uid, hours, i), enable_str))
+
+    params = {
+    "messages" : messages,
+    "pages" : pages,
+    }
+
+    if uid:
+        params["uid"] =  current_uid
+
+    if hours:
+        params["hours"] = current_hours
+
+    return render_to_response('index.html', params)
+
 
