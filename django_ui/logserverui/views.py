1076bfabc87b94a0d15c975ac68a4afb792b23e8
Sarath Lakshman <sarathlakshman@slynux.com>
2013-05-12 19:02:31 +0530

    Add validation checks

--- django_ui/logserverui/views.py
+++ django_ui/logserverui/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render_to_response
 from logserverui.lib.userlog import UserLogFetcher
 from django.conf import settings
+from requests import ConnectionError
 
 page_limit = settings.UI_PAGE_LIMIT
 
@@ -8,18 +9,28 @@ def index(request):
     uid = 0
     page = 0
     hours = 0
+    error = None
 
     current_uid = "user id"
     current_hours = "hours"
 
     if request.GET.has_key('uid'):
-        uid = int(request.GET['uid'])
+        try:
+            uid = int(request.GET['uid'])
+        except:
+            error = "Invalid uid"
 
     if request.GET.has_key('page'):
-        page = int(request.GET['page'])
+        try:
+            page = int(request.GET['page'])
+        except:
+            error = "Invalid page"
 
     if request.GET.has_key('hours'):
-        hours = int(request.GET['hours'])
+        try:
+            hours = int(request.GET['hours'])
+        except:
+            error = "Invalid hours"
 
     if uid:
         current_uid = "%d" %uid
@@ -28,7 +39,14 @@ def index(request):
         current_hours = "%d" %hours
 
     ufetcher = UserLogFetcher(settings.ELASTICSEARCH_URL)
-    count, messages = ufetcher.get_userlog(uid, hours, page*page_limit, page_limit)
+
+    try:
+        count, messages = ufetcher.get_userlog(uid, hours, page*page_limit, page_limit)
+    except ConnectionError,e:
+        error = "Unable to reach elasticsearch server"
+
+    if error:
+        return render_to_response('index.html', {"error": error})
 
     maxpages = count/page_limit
     if count % page_limit:
